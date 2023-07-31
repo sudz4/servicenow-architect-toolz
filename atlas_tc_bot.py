b3fb37571e5110e807bb43906202210e61169d16
sudz4 <mwsuther@syr.edu>
2023-07-30 23:45:05 -0700

    running but needs help

--- atlas_tc_bot.py
+++ atlas_tc_bot.py
@@ -23,8 +23,6 @@ def chatgpt_conversation(conversation_Log):
         model=model_id,
         messages=conversation_Log
     )
-    # print(response)  # print the response object
-
     # extract the role and content from the response message
     role = response.choices[0].message['role']
     content = response.choices[0].message['content']
@@ -39,15 +37,30 @@ conversations = []
 # roles = system, user, assistant
 conversations.append({
     'role': 'system', 
-    'content': 'Hey sudz, I am a ServiceNow Certified Master Architect Assistant.'
+    'content': 'Hey sudz, I am a ServiceNow Certified Master Architect Assistant. I am here to help you manage your daily objectives and generate your daily accomplishments. Please input your objectives, each followed by a new line.'
 })
 conversations = chatgpt_conversation(conversations)
 print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
 
 while True:
     prompt = input('User:')
+    if prompt.strip().lower() == 'quit':
+        break
     conversations.append({'role': 'user', 'content': prompt})
     conversations = chatgpt_conversation(conversations)
     print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
-    if conversations[-1]['content'].strip() == 'quit':
-        break
+
+    # Check if the user input is daily objectives
+    if conversations[-1]['role'] == 'user':
+        objectives = conversations[-1]['content'].split('\n')  # split the objectives by new line
+        for obj in objectives:
+            # Generate a grammar-corrected objective and an accomplishment for each objective
+            conversations.append({'role': 'user', 'content': f'Correct the grammar: {obj}'})
+            conversations = chatgpt_conversation(conversations)
+            corrected_obj = conversations[-1]['content'].strip()
+            print(f'Corrected Objective: {corrected_obj}\n')
+
+            conversations.append({'role': 'user', 'content': f'Generate accomplishment: {corrected_obj}'})
+            conversations = chatgpt_conversation(conversations)
+            accomplishment = conversations[-1]['content'].strip()
+            print(f'Accomplishment: {accomplishment}\n')
